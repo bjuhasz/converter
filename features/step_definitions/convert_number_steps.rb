f7831a41378febc8e39241c247ed25651b8366f4
Bela Juhasz <juhasz.bela@gmail.com>
2012-02-04 21:38:57 +0100

    step defs

---
+++ features/step_definitions/convert_number_steps.rb
@@ -0,0 +1,20 @@
+
+Given /^I go to the new number page$/ do
+end
+
+Given /^I fill in "([^"]*)" with "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I press "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should be on the converted number page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
