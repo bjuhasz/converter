f328e42a4a522fb59c50121a2b0dee7a488264f1
Bela Juhasz <juhasz.bela@gmail.com>
2012-02-04 23:07:50 +0100

    outside-in?

---
+++ spec/views/numbers/convert.html.erb_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "numbers/convert" do
+  before(:each) do
+    @number = assign(:number, stub_model(Number,
+      :number => 1
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    rendered.should match(/Original number:.*12/m)
+    rendered.should match(/Converted number:.*twelve/m)
+  end
+end
