7579b4bbf88db64807eb0288879e9cb051b58af5
Bela Juhasz <juhasz.bela@gmail.com>
2012-02-04 22:17:42 +0100

    cannot fill in, no text field

--- features/step_definitions/convert_number_steps.rb
+++ features/step_definitions/convert_number_steps.rb
@@ -3,8 +3,8 @@ Given /^I go to (.+)$/ do |page_name|
   visit path_to(page_name)
 end
 
-Given /^I fill in "([^"]*)" with "([^"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+Given /^I fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
+  fill_in(field, :with => value)
 end
 
 When /^I press "([^"]*)"$/ do |arg1|
