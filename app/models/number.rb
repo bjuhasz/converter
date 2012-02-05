1ec9249fbd789f83e62c3fe1008c114653552c25
Bela Juhasz <juhasz.bela@gmail.com>
2012-02-05 01:04:25 +0100

    kommenteket atrendeztem

--- app/models/number.rb
+++ app/models/number.rb
@@ -8,15 +8,20 @@ class Number # < ActiveRecord::Base
   attr_accessor :number
 
   validates :number, :presence => true
-  validates :number, :numericality => { :only_integer => true, :message => ": \"%{value}\" is not a number" }
   
+  # TODO
+  # message talan jobb lenne i18n-nel, mert most furcsan nez ki az, hogy:
+  # Number : "1999dd" is not a number
+  # talan szebb lenne ez:
+  # The given input (1999dd) is not a number
+  validates :number, :numericality => { :only_integer => true, :message => ": \"%{value}\" is not a number" }
+
+  # Timothy Byrd's solution, which can be found at: 
+  # http://www.rubyquiz.com/quiz25.html
+  #  
+  # A tobbi az vagy nem nineteen hundredot irt ki, 
+  # vagy nem hasznalta az "and" szocskat.  
   def converted_number
-    # Timothy Byrd's solution, which can be found at: 
-    # http://www.rubyquiz.com/quiz25.html
-    #  
-    # A tobbi az vagy nem nineteen hundredot irt ki, 
-    # vagy nem hasznalta az "and" szocskat.
-    
     EnglishNumerals.to_English(number)
   end
   
