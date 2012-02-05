d95eb400177ac1761dfe8c97b84b300e0af8de13
Bela Juhasz <juhasz.bela@gmail.com>
2012-02-05 01:30:47 +0100

    check for link

--- spec/views/numbers/convert.html.erb_spec.rb
+++ spec/views/numbers/convert.html.erb_spec.rb
@@ -5,11 +5,15 @@ describe "numbers/convert" do
     @number = assign(:number, stub_model(Number,
       :number => 12, :converted_number => 'twelve',
     ))
+    render
   end
 
-  it "renders attributes in <p>" do
-    render
+  it "renders numbers" do
     rendered.should match(/Original number:.*12/m)
     rendered.should match(/Converted number:.*twelve/m)
+  end
+  
+  it "renders link to convert another number" do
+    rendered.should have_link 'Convert another one'
   end
 end
